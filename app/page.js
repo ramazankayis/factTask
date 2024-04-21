8663567f5c0e7a68f3ef2ca148628f5ce6219383
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2024-04-21 22:48:28 +0300

    added datagrid delete and box operation

--- app/page.js
+++ app/page.js
@@ -1,95 +1,109 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+"use client";
+import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { DataGridPro } from "@mui/x-data-grid-pro";
+import Box from "@mui/material/Box";
 export default function Home() {
+  const [facts, setFacts] = useState([]);
+  const [page, setPage] = React.useState(1);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [isShow, setIsShow] = useState(false);
+
+  const [selectedRows, setSelectedRows] = React.useState([]);
+
+  const handleDeleteRow = (id) => {
+    setFacts(facts.filter((fact) => fact.id !== id));
+  };
+  const handleDetailRow = (id) => {
+    setIsShow(true);
+    setSelectedRows(facts.find((fact) => fact.id === id));
+  };
+
+  const fetchData = () => {
+    fetch(`https://catfact.ninja/facts?page=${page}&limit=30`)
+      .then((response) => {
+        return response.json();
+      })
+
+      .then((data) => {
+        data.data = data.data.map((item, index) => ({
+          ...item,
+          id: index + 1,
+        }));
+        console.log("data2", data), setFacts(data.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handlePageChange = (params) => {
+    if (params === page + 1) {
+      setPage(params + 1);
+    } else if (params === page - 1) {
+      setPage(params - 1);
+    }
+  };
+
+  const handlePageSizeChange = () => {
+    setPage(1);
+  };
+  const columns = [
+    { field: "fact", headerName: "fact", width: 300 },
+    { field: "length", headerName: "length", width: 100 },
+    {
+      field: "delete",
+      headerName: "Delete",
+      width: 120,
+      renderCell: (params) => (
+        <button onClick={() => handleDeleteRow(params.row.id)}>Delete</button>
+      ),
+    },
+    {
+      field: "detail",
+      headerName: "Detail",
+      width: 120,
+
+      renderCell: (params) => (
+        <button onClick={() => handleDetailRow(params.row.id)}>Detail</button>
+      ),
+    },
+  ];
+  console.log("array", facts);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGridPro
+        rows={facts}
+        rowCount={50}
+        columns={columns}
+        pagination
+        onPageSizeChange={handlePageSizeChange}
+        onPageChange={(e) => {
+          handlePageChange(e);
+        }}
+        pageSize={() => setRowsPerPage(rowsPerPage)}
+        page={page}
+        filterMode="server"
+        checkboxSelection
+        getRowId={(row) => row.id}
+        {...facts}
+      />
+      {isShow ? (
+        <Box sx={{ p: 2, border: "1px dashed grey" }}>
+          fact : {JSON.stringify(selectedRows.fact, null, 4)}
+          <br />
+          length : {JSON.stringify(selectedRows?.length, null, 4)}
+        </Box>
+      ) : (
+        ""
+      )}
+      {/* <Box sx={{ p: 2, border: "1px dashed grey" }}>
+        fact : {JSON.stringify(selectedRows.fact, null, 4)}
+        <br />
+        length : {JSON.stringify(selectedRows?.length, null, 4)}
+      </Box> */}
+    </div>
   );
 }
